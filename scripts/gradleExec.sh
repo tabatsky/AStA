dd0fb934a8cacf3f3764dc3cd5b9c43d64775330
Evgeny Tabatsky <e.tabatsky@gmail.com>
2017-09-15 15:13:50 +1000

    updated

--- scripts/gradleExec.sh
+++ scripts/gradleExec.sh
@@ -1,5 +1,5 @@
 export JAVA_HOME=/opt/jdk1.8.0_144/
-export ANDROID_HOME=/usr/local/android-sdk/
+export ANDROID_HOME=/opt/android-sdk-linux/
 export PATH=/opt/gradle-3.5/bin:$PATH
 cd $1
 gradle $2
