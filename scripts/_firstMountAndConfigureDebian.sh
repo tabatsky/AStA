b8ba6e5438151b6ba0c2e890814bf478bf7c1a49
Evgeny Tabatsky <e.tabatsky@gmail.com>
2017-09-15 15:50:39 +1000

    updated

--- scripts/_firstMountAndConfigureDebian.sh
+++ scripts/_firstMountAndConfigureDebian.sh
@@ -1,35 +1,54 @@
+# Пароль для доступа по SSH:
 PASSWORD={password}
+# Точка монтирования Debian:
 MNTPT={mntpt}
+# Путь к карте памяти:
 STORAGE={storage}
+# Образ Debian:
 IMG_FILE=$STORAGE/debianOnAndroid.img
+# Версия Debian:
 DEBIAN_VERSION={debianVersion}
+# Создаем точку монтирования:
 mkdir -p $MNTPT
+# Монтируем образ:
 busybox mount -o loop $IMG_FILE $MNTPT
+# Нужно для корректной работы chroot:
 export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/system/bin:/system/xbin:/su/bin:/su/xbin
+# Второй этап debootstrap:
 chroot $MNTPT /debootstrap/debootstrap --second-stage
+# Настраиваем apt:
 echo "deb http://ftp.se.debian.org/debian $DEBIAN_VERSION main contrib non-free" > $MNTPT/etc/apt/sources.list
+# Монтируем:
 busybox mount -t proc none $MNTPT/proc
 busybox mount -t sysfs none $MNTPT/sys
 busybox mount -o bind /dev $MNTPT/dev
 busybox mount -t devpts none $MNTPT/dev/pts
 export TMPDIR=/tmp
 #chroot $MNTPT /bin/bash
+# Чтобы apt мог работать с сетью, нужно перевести его в группу Internet (3003)
 chroot $MNTPT sed -i 's#_apt:x:104:65534::/nonexistent:/bin/false#_apt:x:104:3003::/nonexistent:/bin/false#' /etc/passwd
+# Устанавливаем пакеты, которые нам понадобятся:
 chroot $MNTPT apt-get update
 chroot $MNTPT apt-get --yes upgrade
 chroot $MNTPT apt-get --yes install ne openssh-server unzip \
  android-sdk-build-tools android-sdk-platform-tools
+# Устанавливаем пароль для доступа по SSH:
 echo "echo 'root:$PASSWORD' | chpasswd" > $MNTPT/root/setpasswd.sh
 chroot $MNTPT /bin/sh /root/setpasswd.sh
 cat $MNTPT/root/setpasswd.sh
+# Заменяем строчку в конфиге SSH, чтобы работал доступ под root'ом:
 chroot $MNTPT sed -i '/PermitRootLogin without-password/c\PermitRootLogin yes' /etc/ssh/sshd_config 
 chroot $MNTPT /etc/init.d/ssh restart
+# Создаем папку AStA в Debian и монтируем на нее папку Android:
 mkdir -p $MNTPT/AStA
 busybox mount -o bind $STORAGE/AStA $MNTPT/AStA
+# Имена файлов архивов:
 ANDROID_SDK_TAR_GZ={androidSdkTarGz}
 JDK_8_TAR_GZ={jdk8TarGz}
 GRADLE_ZIP={gradleZip}
+# Распаковываем архивы:
 chroot $MNTPT tar -xzvf /AStA/archives/$ANDROID_SDK_TAR_GZ -C /opt 
 chroot $MNTPT tar -xzvf /AStA/archives/$JDK_8_TAR_GZ -C /opt
 chroot $MNTPT unzip /AStA/archives/$GRADLE_ZIP -d /opt
-exit
+#
+echo "first mount and configure debian done"
--- scripts/_installDebianOnAndroid.sh
+++ scripts/_installDebianOnAndroid.sh
@@ -13,12 +13,19 @@ DATA_DRIVE={dataDrive}
 # размер файла образа не должен превышать (2^32 - 1) байт
 BS=1M
 COUNT=4095
+# Создаем файл образа:
 dd if=/dev/zero of=$DATA_DRIVE/debianOnAndroid.img bs=$BS count=$COUNT
+# Создаем точку монтирования на десктопе:
 mkdir -p ~/debianOnAndroid
+# Создаем на образе файловую систему:
 sudo mkfs.ext3 $DATA_DRIVE/debianOnAndroid.img
+# Монтируем образ:
 sudo mount -o user,loop,exec,dev $DATA_DRIVE/debianOnAndroid.img ~/debianOnAndroid/
+# С помощью debootstrap создаем на образе debian^
 sudo debootstrap --verbose --arch $ARCH --foreign $DEBIAN_VERSION ~/debianOnAndroid/ http://ftp.se.debian.org/debian
+# Размонтируем образ:
 sudo umount $DATA_DRIVE/debianOnAndroid.img
+# Кладем образ на карту памяти:
 adb push $DATA_DRIVE/debianOnAndroid.img $STORAGE
 # Busybox лучше заранее установить через приложение:
 # https://play.google.com/store/apps/details?id=stericson.busybox
@@ -30,6 +37,7 @@ adb push firstMountAndConfigureDebian.sh $STORAGE
 # Скрипты для монтирования и размонтирования
 adb push mountDebian.sh $STORAGE
 adb push umountDebian.sh $STORAGE
+# Создаем папки AStA на Android:
 adb shell mkdir -p $STORAGE/AStA
 adb shell mkdir -p $STORAGE/AStA/Projects
 adb shell mkdir -p $STORAGE/AStA/archives
@@ -38,9 +46,13 @@ adb shell mkdir -p $STORAGE/AStA/scripts
 ANDROID_SDK_TAR_GZ={androidSdkTarGz} 
 JDK_8_TAR_GZ={jdk8TarGz}
 GRADLE_ZIP={gradleZip}
+# Кладем архивы в папку AStA/archives на карте памяти:
 adb push archives/$ANDROID_SDK_TAR_GZ $STORAGE/AStA/archives
 adb push archives/$JDK_8_TAR_GZ $STORAGE/AStA/archives
 adb push archives/$GRADLE_ZIP $STORAGE/AStA/archives
+# Скрипт для запуска команд Gradle:
 adb push gradleExec.sh $STORAGE/AStA/scripts
 # Запускаем скрипт первоначальной настройки Debian:
 adb shell su -c sh $STORAGE/firstMountAndConfigureDebian.sh
+#
+echo "install debian on android done"
